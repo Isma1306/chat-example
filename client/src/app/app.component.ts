ed13712c80ed6dd3987a9daea3ae94d31841dd4f
Isma1306 <isma1306@gmail.com>
2022-07-17 16:49:24 +0200

    Added submitHandler to the chatform

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  messageInput = new FormControl('', Validators.required);
+
+  submitHandler($event: SubmitEvent) {
+    $event.preventDefault();
+    if (!this.messageInput.errors?.['required']) {
+      //send to API service
+    }
+    this.messageInput.reset();
+  }
 }
